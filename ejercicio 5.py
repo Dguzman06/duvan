15c653ed8439a0a0d5778a9e08f2c15980dbbb6b
ASUS <dguzman358@soyudemedellin.edu.co>
2023-08-16 23:26:05 -0500

    Initial commit. add ejercicio 1.py

---
+++ ejercicio 5.py
@@ -0,0 +1,10 @@
+def fahrenheit_a_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+
+
+temperatura_fahrenheit = float(input("Ingrese la temperatura en grados Fahrenheit: "))
+
+
+temperatura_celsius = fahrenheit_a_celsius(temperatura_fahrenheit)
+print("La temperatura en grados Celsius es:", temperatura_celsius)
